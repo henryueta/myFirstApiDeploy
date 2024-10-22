c330ac8da32785cf75233a6be8c15989f1a87500
henryueta <henrykenjiueta@gmail.com>
2024-10-22 09:55:51 -0300

    myfirstcommit

---
+++ src/model/user.js
@@ -0,0 +1,38 @@
+
+const user = [
+
+    {
+        id: 1,
+        nome: "Henry Kenji Ueta",
+        email: "henrykenjiuetagmail.com",
+        data_nascimento: "29/09/2005"
+    },
+    {
+        id: 2,
+        nome: "João Felipe dos Santos",
+        email: "felipejoao@gmail.com",
+        data_nascimento: "01/05/2003"
+    },
+    {
+        id: 3,
+        nome: "Leonardo",
+        email: "leongmail.com",
+        data_nascimento: "11/11/2011"
+    },
+    {
+        id: 4,
+        nome: "Julia",
+        email: "dajuliagmail.com",
+        data_nascimento: "03/10/1998"
+    },
+    {
+        id: 5,
+        nome: "Leonor Ferreira",
+        email: "leonorferreiragmail.com",
+        data_nascimento: "18/07/2004"
+    }
+
+
+]
+
+module.exports = user;
