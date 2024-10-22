9da29d350ed72d7bd3e7e18b7d566734f0db5cbf
henryueta <henrykenjiueta@gmail.com>
2024-10-22 20:55:06 -0300

    api

--- express.js
+++ express.js
@@ -4,6 +4,17 @@ const port = process.env.PORT || 2005;
 
 const app = express();
 
+const home = app.get("/",async(req,res)=>{
+
+    try{
+        await res.status(200).send("SUCESS");
+    }
+    catch(error){
+        res.status(500).send(error);
+    }
+
+})
+
 const server = app.get("/users",async (req,res)=>{
 
     try{
